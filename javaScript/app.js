b21c78aa2086907a061782c3464c2ff65cbdffd6
xCrowly <ahhhhhmedeeeee@gmail.com>
2022-06-29 19:17:10 +0200

    some codes optimisation

--- javaScript/app.js
+++ javaScript/app.js
@@ -9,44 +9,56 @@ const scissorsBut = document.getElementById("scissors");
 
 function getUserChoice(choice) {
     console.log("you choosed " + choice);
-    var ran = randomNumber();
+    var ran = randomChoice();
     // console.log(randomNumber());
-    if (choice === "paper") {
-        if (ran === "scissors") {
-            console.log("You lose, scissors");
-        } else if (ran === "rock") {
-            console.log("You win, rock");
-        } else {
-            console.log("Draw, paper");
-        }
-    } else if (choice === "rock") {
-        if (ran === "scissors") {
-            console.log("You win, scissors");
-        } else if (ran === "rock") {
-            console.log("Draw, rock");
-        } else {
-            console.log("You lose, paper");
-        }
-    } else {
-        if (ran === "scissors") {
-            console.log("Draw, scissors");
-        } else if (ran === "rock") {
-            console.log("You lose, rock");
-        } else {
-            console.log("You win, paper");
-        }
-    }
+    // if (choice === "paper") {
+    //     if (ran === "scissors") {
+    //         console.log("You lose, scissors");
+    //     } else if (ran === "rock") {
+    //         console.log("You win, rock");
+    //     } else {
+    //         console.log("Draw, paper");
+    //     }
+    // } else if (choice === "rock") {
+    //     if (ran === "scissors") {
+    //         console.log("You win, scissors");
+    //     } else if (ran === "rock") {
+    //         console.log("Draw, rock");
+    //     } else {
+    //         console.log("You lose, paper");
+    //     }
+    // } else {
+    //     if (ran === "scissors") {
+    //         console.log("Draw, scissors");
+    //     } else if (ran === "rock") {
+    //         console.log("You lose, rock");
+    //     } else {
+    //         console.log("You win, paper");
+    //     }
+    // }
 
+    switch (randomChoice()) {
+        case "paper":
+            if (ran === "scissors") { console.log("You lose, scissors"); }
+            else if (ran === "rock") { console.log("You win, rock"); }
+            else { console.log("Draw, paper"); }
+            break;
+        case "rock":
+            if (ran === "scissors") { console.log("You win, scissors"); }
+            else if (ran === "rock") { console.log("Draw, rock");; }
+            else { console.log("You lose, paper"); }
+            break;
+        case "scissors":
+            if (ran === "scissors") { console.log("Draw, scissors"); }
+            else if (ran === "rock") { console.log("You lose, rock"); }
+            else { console.log("You win, paper"); }
+            break;
+    }
 }
-function randomNumber() {
-    if (Math.floor(Math.random() * 3) + 1 == 1) {
-        return "rock";
-    } else if (Math.floor(Math.random() * 3) + 1 == 1) {
-        return "paper"
-    } else {
-        return "scissors";
-    }
 
+function randomChoice() {
+    let arr = ["rock", "paper", "scissors"]
+    return arr[Math.floor(Math.random() * 3)];
 }
 
 rockBut.addEventListener('click', function () {
